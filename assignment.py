a36d9b7a2dab760731517e5a838f6f63a492e761
asaadird <masaadirshad@gmail.com>
2023-08-25 11:47:22 +0530

    comiit7

--- assignment.py
+++ assignment.py
@@ -1,80 +1,53 @@
-class Match:
-    def __init__(self, location, team1, team2, timing):
-        self.location = location
-        self.team1 = team1
-        self.team2 = team2
-        self.timing = timing 
 class FlightTable:
-    def __init__(self):
-        self.matches = []
+    def __init__(self, data):
+        self.data = data
 
-    def add_match(self, match):
-        self.matches.append(match)
+    def sort_by_p_id(self):
+        self.data.sort(key=lambda x: x["P_ID"])
 
-    def list_matches_by_team(self, team_name):
-        matches = []
-        for match in self.matches:
-            if team_name in [match.team1, match.team2]:
-                matches.append(match)
-        return matches
-    def list_matches_by_location(self, location):
-        matches = []
-        for match in self.matches:
-            if match.location == location:
-                matches.append(match)
-        return matches
+    def sort_by_start_time(self):
+        self.data.sort(key=lambda x: x["Start Time (ms)"])
 
-    def list_matches_by_timing(self, timing):
-        matches = []
-        for match in self.matches:
-            if match.timing == timing:
-                matches.append(match)
-        return matches
-def main():
-    flight_table = FlightTable()
+    def sort_by_priority(self):
+        priority_order = {"Low": 1, "MID": 2, "High": 3}
+        self.data.sort(key=lambda x: priority_order[x["Priority"]])
 
-    flight_table.add_match(Match("Mumbai", "India", "Sri Lanka", "DAY"))
-    flight_table.add_match(Match("Delhi", "England", "Australia", "DAY-NIGHT"))
-    flight_table.add_match(Match("Chennai", "India", "South Africa", "DAY"))
-    flight_table.add_match(Match("Indore", "England", "Sri Lanka", "DAY-NIGHT"))
-    flight_table.add_match(Match("Mohali", "Australia", "South Africa", "DAY-NIGHT"))
-    flight_table.add_match(Match("Delhi", "India", "Australia", "DAY"))
-
-    while True:
-        print("\nSearch Options:")
-        print("1. List of all matches of a Team")
-        print("2. List of Matches on a Location")
-        print("3. List of Matches based on timing")
-        print("4. Exit")
-
-        choice = input("Enter your choice: ")
-        
-        if choice == "1":
-            team_name = input("Enter the team name: ")
-            matches = flight_table.list_matches_by_team(team_name)
-            print("Matches involving", team_name)
-            for match in matches:
-                print(match.location, match.team1, "vs", match.team2, match.timing)
-
-        elif choice == "2":
-            location = input("Enter the location: ")
-            matches = flight_table.list_matches_by_location(location)
-            print("Matches in", location)
-            for match in matches:
-                print(match.team1, "vs", match.team2, match.timing)
-
-        elif choice == "3":
-            timing = input("Enter the timing: ")
-            matches = flight_table.list_matches_by_timing(timing)
-            print("Matches with timing", timing)
-            for match in matches:
-                print(match.location, match.team1, "vs", match.team2)
-        elif choice == "4":
-            print("Exiting the program.")
-            break
-
-        else:
-            print("Invalid choice. Please select a valid option.")
+    def print_table(self):
+        print("{:<5} {:<10} {:<18} {}".format("P_ID", "Process", "Start Time (ms)", "Priority"))
+        print("=" * 50)
+        for row in self.data:
+            print("{:<5} {:<10} {:<18} {}".format(row["P_ID"], row["Process"], row["Start Time (ms)"], row["Priority"]))
 
 if __name__ == "__main__":
-    main()
+    data = [
+        {"P_ID": "P1", "Process": "VSCode", "Start Time (ms)": 100, "Priority": "MID"},
+        {"P_ID": "P23", "Process": "Eclipse", "Start Time (ms)": 234, "Priority": "MID"},
+        {"P_ID": "P93", "Process": "Chrome", "Start Time (ms)": 189, "Priority": "High"},
+        {"P_ID": "P42", "Process": "JDK", "Start Time (ms)": 9, "Priority": "High"},
+        {"P_ID": "P9", "Process": "CMD", "Start Time (ms)": 7, "Priority": "High"},
+        {"P_ID": "P87", "Process": "NotePad", "Start Time (ms)": 23, "Priority": "Low"}
+    ]
+
+    flight_table = FlightTable(data)
+
+    while True:
+        print("\nSelect sorting parameter:")
+        print("1. Sort by P_ID")
+        print("2. Sort by Start Time")
+        print("3. Sort by Priority")
+        print("0. Exit")
+        choice = int(input("Enter your choice: "))
+
+        if choice == 1:
+            flight_table.sort_by_p_id()
+        elif choice == 2:
+            flight_table.sort_by_start_time()
+        elif choice == 3:
+            flight_table.sort_by_priority()
+        elif choice == 0:
+            print("Exiting...")
+            break
+        else:
+            print("Invalid choice. Please choose again.")
+
+        flight_table.print_table()
